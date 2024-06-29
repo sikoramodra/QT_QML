9db5b59cc77fda4386c4d1a716a15e7754aa91ed
sikoramodra <wojciechmodro@gmail.com>
2024-06-29 13:04:11 +0200

    Qt policy QTP0001 changed to NEW

--- test_project/main.cpp
+++ test_project/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/test_project/Main.qml"));
+    const QUrl url(QStringLiteral("qrc:/qt/qml/test_project/Main.qml"));
     QObject::connect(
             &engine,
             &QQmlApplicationEngine::objectCreationFailed,
